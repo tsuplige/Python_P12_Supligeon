953440c803704555f10d63c8cf84134ab068fee3
Tsuplige <thomassupligeon@gmail.com>
2023-12-31 00:06:08 +0100

    add urls

--- EpicEventsAPI/EpicEventsAPI/urls.py
+++ EpicEventsAPI/EpicEventsAPI/urls.py
@@ -4,17 +4,23 @@ from django.contrib import admin
 # from rest_framework_simplejwt.views import (TokenObtainPairView,
 #                                             TokenRefreshView)
 
-from authentication import views
+from authentication.views import UserViewSet
+from api.views import ClientViewset, ContractViewset, EventViewset
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
+router = routers.SimpleRouter()
+
+router.register(r'clients', ClientViewset, basename='clients')
+router.register(r'contracts', ContractViewset, basename='contract')
+router.register(r'events', EventViewset, basename='event')
+router.register(r'users',  UserViewSet, basename='users')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls',
+                              namespace='rest_framework'))
 ]
 
 urlpatterns += router.urls
